44aa741582e005692f903e233f50f52417e393dd
alexscub <alexscub1@gmail.com>
2019-11-30 16:45:30 +0200

    Updates

---
+++ precache-manifest.a757c074c0bcb8987d6462ebe94f3a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522cf542c65a7daee85de9ac5ce7df3a",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "6a9590f15e7a5283d00e",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.33bd9b1c.chunk.css"
+  },
+  {
+    "revision": "122166fdaf41461ca23a",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.6b3d8093.chunk.js"
+  },
+  {
+    "revision": "6a9590f15e7a5283d00e",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.fc4064cc.chunk.js"
+  },
+  {
+    "revision": "93bf45fbd1399556846a",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.eba5c734.js"
+  }
+]);
